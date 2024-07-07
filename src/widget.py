3f1acf1170852c6633d81f94b17a03c48ef69838
Dmitri Muskatin <muskatin@gmail.com>
2024-07-07 16:00:59 +0800

    new_commit

--- src/widget.py
+++ src/widget.py
@@ -3,6 +3,8 @@ from masks import mask_card, mask_account
 
 def mask_account_card(acc_number: str) -> str:
     """Возвращет исходную строку с замаскированным номером карты/счета"""
+
+
     pay_system = ("Visa", "Maestro", "MasterCard")
     for i in acc_number.split():
         if i in pay_system:
@@ -16,6 +18,8 @@ def mask_account_card(acc_number: str) -> str:
 
 def get_date(date_and_time: str) -> str:
     """строку с датой в виде дд.мм.гггг"""
+
+
     date = [i for i in date_and_time.split("T")]
     return ".".join(list(reversed([i for i in date[0].split("-")])))
 
